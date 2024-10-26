7c056ccfcd2b9873b9b8d13b8abcaac7ad5738b2
Navya <navyachinnu037@gmil.com>
2024-10-26 13:58:43 +0530

    changed the application

--- src/main/java/com/javatechie/crud/SpringbootCrudK8sExampleApplication.java
+++ src/main/java/com/javatechie/crud/SpringbootCrudK8sExampleApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringbootCrudK8sExampleApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) {//crud application javan;hgchvgh
 		SpringApplication.run(SpringbootCrudK8sExampleApplication.class, args);
 	}
 
